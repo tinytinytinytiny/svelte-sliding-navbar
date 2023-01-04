d79935500b692600a23a4a3b37f5763564aed02a
tinytinytinytiny <tinynametaken@yahoo.com>
2023-01-04 18:30:17 -0500

    commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# svelte-sliding-navbar
+<https://profound-custard-a03579.netlify.app/>  
+responsive side sheet menu that pushes the website content to the side as it opens
+
+![demo](demo.gif)
+
+## Initialization
+add `initial-scale=1` to meta viewport tag to prevent dpi-related bugs with css transform animation:
+
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+
+prevent horizontal overflow:
+
+	html, body {
+		overflow-x: hidden;
+		overflow-x: -moz-hidden-unscrollable;
+		overflow-x: clip;
+	}
+
+use `text-size-adjust` property to prevent font size from scaling in landscape orientation:
+
+	html {
+		-webkit-text-size-adjust: none;
+		text-size-adjust: none;
+	}
+
+reset all elements to border-box and add transitions:
+
+	*, *::before, *::after {
+		box-sizing: border-box;
+		transition: transform 0.3s ease;
+	}
+
+## Layout
+	<body data-sveltekit-preload-data="hover">
+		<div class="layout">
+			<button
+				aria-controls="main-menu"
+				aria-expanded="false"
+				aria-label="Open menu"
+				class="nav-toggler"
+			>
+			</button>
+			<nav id="main-menu" aria-label="Main menu"></nav>
+			<main></main>
+		</div>
+	</body>
+
+the `.layout` class will be a 3-column grid to create a sidebar layout with the body content centered in the viewport:
+
+	.layout {
+		--max-content-width: 50rem;
+		--nav-width: 18rem;
+		display: grid;
+		grid-template-columns: 1fr minmax(auto, var(--max-content-width)) 1fr;
+	}
+
+	nav {
+		height: 100%;
+		width: var(--nav-width);
+	}
+
+	.nav-toggler {
+		display: none;
+		position: fixed;
+		top: 0;
+		z-index: 20;
+	}
+
+at the breakpoint of **50rem** viewport width, the layout stops being a grid and the body content fills the entire viewport. the navigation menu is pushed to the left:
+
+	@media (max-width: 50rem) {
+		.layout {
+			display: contents;
+		}
+
+		nav {
+			position: fixed;
+			top: 0;
+			transform: translateX(calc(-1 * var(--nav-width)));
+			z-index: 10;
+		}
+
+		.nav-toggler {
+			display: block;
+		}
+	}
+
+## Interactivity
+use store to share nav component’s open state with the entire page:
+
+stores.js
+
+	import { writable } from 'svelte/store';
+
+	export const open = writable(false);
+
+Nav.svelte
+
+	<script>
+		import { open } from './stores.js';
+		import links from '$lib/data/links.json';
+
+		let navOpen;
+
+		open.subscribe(value => {
+			navOpen = value;
+		});
+
+		function handleNavToggle() {
+			open.update(n => !n);
+		}
+	</script>
+
+	<button
+		aria-controls="main-menu"
+		aria-expanded={navOpen}
+		aria-label={navOpen ? 'Close menu' : 'Open menu'}
+		class="nav-toggler"
+		on:click={handleNavToggle}
+	>
+	<nav
+		id="main-menu"
+		aria-label="Main menu"
+		data-state={navOpen ? 'nav-expanded' : 'nav-collapsed'}
+	>
+	</nav>
+
++page.svelte
+
+	<script>
+		import Nav from '$lib/Nav/Nav.svelte';
+		import { open } from '$lib/Nav/stores.js';
+
+		let navOpen;
+
+		open.subscribe(value => {
+			navOpen = value;
+		});
+	</script>
+
+	<Nav />
+	<main data-state={navOpen ? 'nav-expanded' : 'nav-collapsed'}></main>
+
+then, adjust css transforms for menu open state:
+
+	@media (max-width: 50rem) {
+		main[data-state="nav-expanded"] {
+			transform: translateX(var(--nav-width));
+		}
+
+		nav[data-state="nav-expanded"] {
+			transform: none;
+		}
+	}
---
+++ src/lib/Nav/Nav.svelte
@@ -0,0 +1,196 @@
+<script>
+	import { open } from './stores.js';
+	import links from '$lib/data/links.json';
+
+	let navOpen;
+
+	open.subscribe(value => {
+		navOpen = value;
+	});
+
+	function handleNavToggle() {
+		open.update(n => !n);
+	}
+</script>
+
+<button
+	aria-controls="main-menu"
+	aria-expanded={navOpen}
+	aria-label={navOpen ? 'Close menu' : 'Open menu'}
+	class="nav-toggler"
+	on:click={handleNavToggle}
+>
+	<svg aria-hidden="true" class="ham hamRotate ham1" viewBox="0 0 100 100" width="48" data-state={navOpen ? 'close' : 'burger'}>
+		<path class="line top" d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
+		<path class="line middle" d="m 30,50 h 40" />
+		<path class="line bottom" d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
+	</svg>
+</button>
+<nav id="main-menu" aria-label="Main menu" data-state={navOpen ? 'nav-expanded' : 'nav-collapsed'}>
+	<div class="nav-inner">
+		<div class="nav-body">
+			<div class="nav-logo">
+				<img src="/favicon.png" width="128" height="128" alt="">
+			</div>
+			<h2>Sliding Navbar Demo</h2>
+			<ul>
+				{#each links as { title, url }}
+					<li><a href={url}>{title}</a></li>
+				{/each}
+			</ul>
+			<footer>
+				Copyright (c) 2023 Copyright Holder All Rights Reserved.
+			</footer>
+		</div>
+	</div>
+</nav>
+
+<style>
+	nav {
+		height: 100%;
+		width: var(--nav-width);
+	}
+
+	.nav-inner {
+		height: 100%;
+		width: 100%;
+	}
+
+	.nav-body {
+		max-height: 100vh;
+		max-height: 100dvh;
+		padding: var(--padding);
+		padding-bottom: calc(var(--padding) + env(safe-area-inset-bottom));
+		position: sticky;
+		overflow-y: auto;
+		overscroll-behavior: contain;
+		top: 0;
+	}
+
+	.nav-toggler {
+		box-sizing: content-box;
+		display: none;
+		height: var(--nav-button-size);
+		margin-inline-start: -12px;
+		padding: var(--padding);
+		position: fixed;
+		top: 0;
+		width: var(--nav-button-size);
+		z-index: 20;
+	}
+
+	.nav-logo {
+		background-color: var(--bg);
+	}
+
+	.nav-logo img {
+		mix-blend-mode: multiply;
+	}
+
+	nav h2 {
+		margin-block-start: var(--space-xs);
+	}
+
+	nav ul {
+		list-style: none;
+		padding: 0;
+	}
+
+	nav ul::before,
+	nav footer::before {
+		content: '';
+		background-color: rgba(0, 0, 0, 0.16);
+		display: block;
+		height: 1px;
+		margin-block: var(--stack-space);
+		width: 100%;
+	}
+
+	nav a {
+		color: inherit;
+		display: block;
+		padding-block: calc(var(--space-s) / 2);
+		position: relative;
+		text-decoration: none;
+	}
+
+	nav a::before {
+		content: '';
+		inset: 0 calc(-1 * var(--padding));
+		position: absolute;
+	}
+
+	nav a:hover::before {
+		background-color: rgba(0, 0, 0, 0.08);
+	}
+
+	.hamRotate[data-state="close"] {
+		transform: rotate(45deg);
+	}
+
+	.line {
+		fill: none;
+		transition: stroke-dasharray 400ms, stroke-dashoffset 400ms;
+		stroke: currentColor;
+		stroke-width: 5.5;
+		stroke-linecap: round;
+	}
+
+	.ham1 .top {
+		stroke-dasharray: 40 139;
+	}
+
+	.ham1 .bottom {
+		stroke-dasharray: 40 180;
+	}
+
+	.ham1[data-state="close"] .top {
+		stroke-dashoffset: -98px;
+	}
+
+	.ham1[data-state="close"] .bottom {
+		stroke-dashoffset: -138px;
+	}
+
+	@media (max-width: 50rem) {
+		nav {
+			position: fixed;
+			top: 0;
+			transform: translateX(calc(-1 * var(--nav-width)));
+			z-index: 10;
+		}
+
+		nav[data-state="nav-expanded"] {
+			transform: none;
+		}
+
+		.nav-inner {
+			background-color: var(--bg);
+		}
+
+		.nav-body {
+			padding-block-start: calc(var(--padding) + var(--nav-button-size) + var(--stack-space));
+		}
+
+		nav::before {
+			background-color: rgba(0, 0, 0, 0.05);
+			background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.7) calc(var(--nav-width) / 2), rgba(0, 0, 0, 0.25) calc(var(--nav-width) * 2));
+			content: '';
+			height: 100vh;
+			height: 100lvh;
+			opacity: 0;
+			position: fixed;
+			width: calc(100vw + var(--nav-width));
+			width: calc(100lvw + var(--nav-width));
+			z-index: -1;
+		}
+
+		nav[data-state="nav-expanded"]::before {
+			opacity: 1;
+		}
+
+		.nav-toggler {
+			display: block;
+		}
+	}
+</style>
